8aa0997c15617b40b40f17c40c2242ed897b9a43
Yazid <hello@sweftat.com>
2026-03-22 08:29:20 +0300

    fix corrupted files, admin routes, middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function middleware(req: NextRequest) {
+export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
-
   if (pathname.startsWith('/admin')) {
     const userId = req.cookies.get('wiispr_user_id')?.value
-    if (!userId) {
-      return NextResponse.redirect(new URL('/', req.url))
-    }
+    if (!userId) return NextResponse.redirect(new URL('/', req.url))
   }
-
   return NextResponse.next()
 }
 
